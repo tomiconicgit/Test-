d40494949e034115a33885512c97504ff20dae2f
tomiconicgit <tomiconicx@icloud.com>
2025-10-19 17:08:26 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,8 @@
+// Main.js — manifest only
+export const FILES = [
+  './src/Game.js',
+  './src/Blocks.js',
+  './src/VoxelWorld.js',
+  './src/controls/FirstPersonMobile.js',
+  './src/ui/HUD.js'
+];
