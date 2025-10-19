58913cd8b7ea84733730b1f3b5190a7dd8d01aad
tomiconicgit <tomiconicx@icloud.com>
2025-10-19 22:41:39 +0100

    Delete Main.js

--- Main.js
+++
@@ -1,8 +0,0 @@
-// Main.js — manifest only
-export const FILES = [
-  './src/Game.js',
-  './src/Blocks.js',
-  './src/VoxelWorld.js',
-  './src/controls/FirstPersonMobile.js',
-  './src/ui/HUD.js'
-];
